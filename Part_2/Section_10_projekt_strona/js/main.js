29b27bac30001ebff8139a5d2259805f535b0e52
Greg <xbravadox@gmail.com>
2021-02-18 19:33:32 +0100

    web site project - finish

--- Part_2/Section_10_projekt_strona/js/main.js
+++ Part_2/Section_10_projekt_strona/js/main.js
@@ -2,10 +2,10 @@ document.addEventListener('DOMContentLoaded', () => {
 
    const nav = document.querySelector('.navbar')
    const hamburger = document.querySelector('.fa-bars')
-
+   // dodaje cień na nawigacji
    function addShadow() {
 
-      if (window.scrollY >= 200) {
+      if (window.scrollY >= 100) {
          nav.classList.add('shadow-bg')
       } else {
          nav.classList.remove('shadow-bg')
@@ -14,13 +14,22 @@ document.addEventListener('DOMContentLoaded', () => {
 
    window.addEventListener('scroll', addShadow)
 
-   $(document).ready(function () {
-      $(document).click(function (event) {
-         var clickover = $(event.target)
-         var _opened = $(".navbar-collapse").hasClass("show")
-         if (_opened === true && !clickover.hasClass("navbar-toggler")) {
-            $(".navbar-toggler").click()
-         }
-      })
+   // skrypt naprawiający nawigację (zamykanie na mobil)
+   $(document).click(function (event) {
+      var clickover = $(event.target)
+      var _opened = $(".navbar-collapse").hasClass("show")
+      if (_opened === true && !clickover.hasClass("navbar-toggler")) {
+         $(".navbar-toggler").click()
+      }
    })
+
+   // slick.js
+   $('.team-carousel').slick({
+      // setting- name: setting - value
+      autoplay: true,
+      autoplaySpeed: 3500,
+      slidesToShow: 4,
+      slidesToScroll: 1
+   })
+  
 })
