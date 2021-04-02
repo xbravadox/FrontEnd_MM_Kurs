ebfece77b3bf5ab11143fb53f15c32fdf2ac0e4c
Greg <xbravadox@gmail.com>
2021-04-02 19:03:29 +0200

    ToDo part 7 finish

--- Part_3/Section_11_projekty/Projekt_6_ToDo/main.js
+++ Part_3/Section_11_projekty/Projekt_6_ToDo/main.js
@@ -9,90 +9,109 @@ let $editedTodo;  // edytowane zadanie
 let $popupInput;  // miejsc wpisywania zadania w popupie
 let $addPopupBtn; // zatwierdzanie zmian w popupie
 let $closeTodoBtn;// button zamykający popup
+let $idNumber = 0;// id zadania
 
 const main = () => {
-   prepareDomElements();
-   prepareDomEvents();
+	prepareDomElements();
+	prepareDomEvents();
 };
 
 // pobieranie elementów
 const prepareDomElements = () => {
-   $todoInput = document.querySelector('.todoInput');
-   $alertInfo = document.querySelector('.alertInfo');
-   $addBtn = document.querySelector('.addBtn');
-   $ulList = document.querySelector('.todoList ul');
-   $popup = document.querySelector('.popup');
-   $popupInfo = document.querySelector('.popupInfo');
-   $popupInput = document.querySelector('.popupInput');
-   $addPopupBtn = document.querySelector('.accept');
-   $closeTodoBtn = document.querySelector('.cancel');
+	$todoInput = document.querySelector('.todoInput');
+	$alertInfo = document.querySelector('.alertInfo');
+	$addBtn = document.querySelector('.addBtn');
+	$ulList = document.querySelector('.todoList ul');
+	$popup = document.querySelector('.popup');
+	$popupInfo = document.querySelector('.popupInfo');
+	$popupInput = document.querySelector('.popupInput');
+	$addPopupBtn = document.querySelector('.accept');
+	$closeTodoBtn = document.querySelector('.cancel');
 };
 
 // tworzenie nowego zadania
 const addNewTask = () => {
-   if ($todoInput.value !== "") {
-      $newTask = document.createElement('li');
-      $newTask.innerText = $todoInput.value;
-      $ulList.appendChild($newTask);
-      $todoInput.value = '';
-      $alertInfo.innerText = '';
-      createToolsArea();
-   } else {
-      $alertInfo.innerText = 'Wpisz treść zadania!';
-   };
+	if ($todoInput.value !== "") {
+		$idNumber++;
+		$newTask = document.createElement('li');
+		$newTask.innerText = $todoInput.value;
+		$newTask.setAttribute('id', `todo-${$idNumber}`);
+		$ulList.appendChild($newTask);
+		$todoInput.value = '';
+		$alertInfo.innerText = '';
+		createToolsArea();
+	} else {
+		$alertInfo.innerText = 'Wpisz treść zadania!';
+	};
 };
 
 // tworzenie diva z narzędziami
 const createToolsArea = () => {
-   const toolsPanel = document.createElement('div');
-   toolsPanel.classList.add('tools');
-   $newTask.appendChild(toolsPanel);
-   ;
-   const completeBtn = document.createElement('button')
-   completeBtn.innerHTML = '<i class="fas fa-check"></i>';
-   completeBtn.classList.add('complete');
+	const toolsPanel = document.createElement('div');
+	toolsPanel.classList.add('tools');
+	$newTask.appendChild(toolsPanel);
+	
+	const completeBtn = document.createElement('button');
+	completeBtn.innerHTML = '<i class="fas fa-check"></i>';
+	completeBtn.classList.add('complete');
 
-   const editBtn = document.createElement('button');
-   editBtn.innerText = 'EDIT';
-   editBtn.classList.add('edit');
+	const editBtn = document.createElement('button');
+	editBtn.innerText = 'EDIT';
+	editBtn.classList.add('edit');
 
-   const deleteBtn = document.createElement('button');
-   deleteBtn.innerHTML = '<i class="fas fa-times"></i>';
-   deleteBtn.classList.add('delete');
+	const deleteBtn = document.createElement('button');
+	deleteBtn.innerHTML = '<i class="fas fa-times"></i>';
+	deleteBtn.classList.add('delete');
 
-   toolsPanel.appendChild(completeBtn);
-   toolsPanel.appendChild(editBtn);
-   toolsPanel.appendChild(deleteBtn);
+	toolsPanel.appendChild(completeBtn);
+	toolsPanel.appendChild(editBtn);
+	toolsPanel.appendChild(deleteBtn);
 };
 
 // nasłuchiwanie zdarzeń
 const prepareDomEvents = () => {
-   $addBtn.addEventListener('click', addNewTask);
-   $ulList.addEventListener('click', checkClick)
-   $closeTodoBtn.addEventListener('click', closePopup)
+	$addBtn.addEventListener('click', addNewTask);
+	$ulList.addEventListener('click', checkClick);
+	$closeTodoBtn.addEventListener('click', closePopup);
+	$addPopupBtn.addEventListener('click', changeTodo);
 };
 
 // obsługa kliknięcia w div tools
 const checkClick = (e) => {
-   if (e.target.closest('button').classList.contains('complete')) {
-      e.target.closest('li').classList.toggle('completed');
-      e.target.closest('button').classList.toggle('completed')
-   } else if (e.target.closest('button').className === 'edit') {
-      editTask();
-   } else if (e.target.closest('button').className === 'delete') {
-      console.log(e.target);
-   }
+	if (e.target.closest('button').classList.contains('complete')) {
+		e.target.closest('li').classList.toggle('completed');
+		e.target.closest('button').classList.toggle('completed');
+	} else if (e.target.closest('button').className === 'edit') {
+		editTask(e);
+	} else if (e.target.closest('button').className === 'delete') {
+		console.log(e.target);
+	}
 };
 
 // edycja zadania w popupie
-const editTask = () => {
-   $popup.style.display = 'flex'
+const editTask = (e) => {
+	const oldTodo = e.target.closest('li').id;
+	$editedTodo = document.getElementById(oldTodo);
+	$popupInput.value = $editedTodo.firstChild.textContent;
+	$popup.style.display = 'flex';
+};
+
+// zmiana treści zadania
+const changeTodo = () => {
+	if ($popupInput.value !== '') {
+		$editedTodo.firstChild.textContent = $popupInput.value;
+		$popup.style.display = 'none';
+		$popupInfo.innerText = '';
+	} else {
+		$popupInfo.innerText = 'Wpisz treść zadania!';
+	};
 };
 
 // zamykanie popupu
-const closePopup = () =>{
-   $popup.style.display = 'none'
-}
+const closePopup = () => {
+	$popupInfo.innerText = '';
+	$popup.style.display = 'none';
+};
 
 
 document.addEventListener('DOMContentLoaded', main);
